d2cb452b373ae13a1a3769f0584e9d254789d31d
JanoiBJR21 <apichat26870@gmail.com>
2024-02-05 22:16:55 +0700

    commit

--- React-1/src/components/StudentItem.jsx
+++ React-1/src/components/StudentItem.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import './StudentItem.css'
 function StudentItem(){
+    // const resulte = false;
+    // const message1 = "Hi Apichat"
+    // const message2 = "Bye Wongtala"
+
+    // const arr1 = [1, 2, 3]
+
+    // let isShow = true;
+    // let hiMessage = "Hi Apichat"
+
+    // let arr2 = ["Hello", "World"]
+    // let arr3 = [""]
+
+    // let title = "ApiCHAt"
+
+    let name = "Apichat";
+    let surname = "Wongtala";
+    let age = "27";
     return (
         <div className="StudentItem">
-            <div>Apichat</div>
-            <div>Wongtala</div>
-            <div>27</div>
+            {/* <div>{resulte ? message1 : message2}</div> */}
+            {/* <div>{arr1}</div> */}
+            {/* <div>{isShow && hiMessage}</div> */}
+            {/* <div>{arr2}</div> */}
+            {/* <div>{arr3.length > 0 && arr3}</div> */}
+            {/* <div>{5 + 5}</div> */}
+            {/* <div>{5 + 5}</div> */}
+            {/* <div>{title.toUpperCase()}</div> */}
+            <div>{name}</div>
+            <div>{surname}</div>
+            <div>{age}</div>
         </div>
     )
 }
