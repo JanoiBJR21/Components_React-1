d8ff75999080686bfdebe91ca68b39332a6732dd
JanoiBJR21 <apichat26870@gmail.com>
2024-02-05 17:04:09 +0700

    commit

---
+++ React-1/src/components/StudentItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './StudentItem.css'
+function StudentItem(){
+    return (
+        <div className="StudentItem">
+            <div>Apichat</div>
+            <div>Wongtala</div>
+            <div>27</div>
+        </div>
+    )
+}
+export default StudentItem;
