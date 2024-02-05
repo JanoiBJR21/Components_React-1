8bfe63925e75d1de1b66f292a8834131d9c20779
JanoiBJR21 <apichat26870@gmail.com>
2024-02-06 01:18:49 +0700

    commit

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -1,42 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import StudentItem from './components/StudentItem'
-import StudentItem2 from './components/StudentItem2'
-import StudentExsample from './components/StudentExsample'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import StudentItem from "./components/StudentItem";
+import StudentItem2 from "./components/StudentItem2";
+import StudentExsample from "./components/StudentExsample";
 
 function App() {
-// const student1 = {name: "Jar", surname: "nofy", age: 27};
-// const student2 = {name: "dar", surname: "goty", age: 30};
-// const student3 = {name: "mor", surname: "city", age: 45};
-// const student4 = {name: "fer", surname: "nika", age: 30};
+  // const student1 = {name: "Jar", surname: "nofy", age: 27};
+  // const student2 = {name: "dar", surname: "goty", age: 30};
+  // const student3 = {name: "mor", surname: "city", age: 45};
+  // const student4 = {name: "fer", surname: "nika", age: 30};
 
-const studentList = [
-  {name: "Jar", surname: "nofy", age: 27}, //studentList[0]
-  {name: "dar", surname: "goty", age: 30}, //studentList[1]
-  {name: "mor", surname: "city", age: 45}, //studentList[2]
-  {name: "fer", surname: "nika", age: 35}  //studentList[3]
-]
+  const studentList = [
+    { name: "Jar", surname: "nofy", age: 29 }, //studentList[0]
+    { name: "dar", surname: "goty", age: 65 }, //studentList[1]
+    { name: "mor", surname: "city", age: 68 }, //studentList[2]
+    { name: "fer", surname: "nika", age: 35 }, //studentList[3]
+  ];
 
   return (
-      <div className="App">
-        <h1>StudentItem</h1>
-        {/* <StudentItem name={student1.name} surname={student1.surname} age={student1.age}/>
+    <div className="App">
+      <h1>StudentItem</h1>
+      {/* <StudentItem name={student1.name} surname={student1.surname} age={student1.age}/>
         <StudentItem name={student2.name} surname={student2.surname} age={student2.age}/>
         <StudentItem name={student3.name} surname={student3.surname} age={student3.age}/>
         <StudentItem name={student4.name} surname={student4.surname} age={student4.age}/> */}
 
-        <StudentItem name={studentList[0].name} surname={studentList[0].surname} age={studentList[0].age}/>
-        <StudentItem name={studentList[1].name} surname={studentList[1].surname} age={studentList[1].age}/>
-        <StudentItem name={studentList[2].name} surname={studentList[2].surname} age={studentList[2].age}/>
-        <StudentItem name={studentList[3].name} surname={studentList[3].surname} age={studentList[3].age}/>
-        <h1>StudentExsample</h1>
-        <StudentExsample>
-          <div>test-1</div>
-          <div>test-2</div>
-        </StudentExsample>
-      </div>
+      <StudentItem
+        name={studentList[0].name}
+        surname={studentList[0].surname}
+        age={studentList[0].age}
+      />
+      <StudentItem
+        name={studentList[1].name}
+        surname={studentList[1].surname}
+        age={studentList[1].age}
+      />
+      <StudentItem
+        name={studentList[2].name}
+        surname={studentList[2].surname}
+        age={studentList[2].age}
+      />
+      <StudentItem
+        name={studentList[3].name}
+        surname={studentList[3].surname}
+        age={studentList[3].age}
+      />
+      <h1>StudentExsample</h1>
+      <StudentExsample>
+        <div>test-1</div>
+        <div>test-2</div>
+      </StudentExsample>
+    </div>
   );
 }
 export default App;
--- React-1/src/components/StudentItem.jsx
+++ React-1/src/components/StudentItem.jsx
@@ -1,52 +1,38 @@
 import React from "react";
-import './StudentItem.css';
+import "./StudentItem.css";
+import StudentTag from "./StudentTag";
 // function StudentItem(){//ฟังก์ชันแบบปกติ
 // const StudentItem = () =>{ //ฟังก์ชันแบบแอโรฟังก์ชัน
-function StudentItem(props){
-    // const resulte = false;
-    // const message1 = "Hi Apichat"
-    // const message2 = "Bye Wongtala"
-
-    // const arr1 = [1, 2, 3]
-
-    // let isShow = true;
-    // let hiMessage = "Hi Apichat"
-
-    // let arr2 = ["Hello", "World"]
-    // let arr3 = [""]
-
-    // let title = "ApiCHAt"
-
-        const name = props.name;
-        const surname = props.surname;
-        const age = props.age;
-
-        let retired;
-        if (age > 30) {
-            retired = "อายุเกิน ";
-        }
-        else{
-            if (age == 30) {
-                retired = "อายุพอดี ";
-            }
-            else{
-                retired = "อายุไม่ถึง";
-            }
-        }
-        return (
-            <div className="StudentItem">
-                {/* <div>{resulte ? message1 : message2}</div> */}
-                {/* <div>{arr1}</div> */}
-                {/* <div>{isShow && hiMessage}</div> */}
-                {/* <div>{arr2}</div> */}
-                {/* <div>{arr3.length > 0 && arr3}</div> */}
-                {/* <div>{5 + 5}</div> */}
-                {/* <div>{title.toUpperCase()}</div> */}
-                <div>{name}</div>
-                <div>{surname}</div>
-                <div>{age}</div>
-                <div>{retired}</div>
-            </div>
-        );
-    }
+function StudentItem(props) {
+  const name = props.name;
+  const surname = props.surname;
+  const age = props.age;
+  // if (age > 30) {
+  //     retired = "อายุเกิน ";
+  // }
+  // else{
+  //     if (age == 30) {
+  //         retired = "อายุพอดี ";
+  //     }
+  //     else{
+  //         retired = "อายุไม่ถึง";
+  //     }
+  // }
+  return (
+    <div className="StudentItem">
+      {/* <div>{resulte ? message1 : message2}</div> */}
+      {/* <div>{arr1}</div> */}
+      {/* <div>{isShow && hiMessage}</div> */}
+      {/* <div>{arr2}</div> */}
+      {/* <div>{arr3.length > 0 && arr3}</div> */}
+      {/* <div>{5 + 5}</div> */}
+      {/* <div>{title.toUpperCase()}</div> */}
+      <div>{name}</div>
+      <div>{surname}</div>
+      <div>{age}</div>
+      {/* <div>{retired}</div> */}
+      <StudentTag tagAge={age} />
+    </div>
+  );
+}
 export default StudentItem;
